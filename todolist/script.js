5570d2c8576e10e60984d802474857ce46f563dd
Yana Kapustian <65828184+YanaKapustian@users.noreply.github.com>
2021-10-29 22:29:37 +0300

    todolist

---
+++ todolist/script.js
@@ -0,0 +1,65 @@
+const addMessage = document.querySelector('.message')
+const addButton = document.querySelector('.add')
+const todo = document.querySelector('.todo')
+
+let todoList = []
+
+if (localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'))
+    displayMessages()
+}
+
+addButton.addEventListener('click', function(){
+    if (!addMessage.value) return
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    }
+    todoList.push(newTodo)
+    displayMessages()
+    localStorage.setItem('todo', JSON.stringify(todoList))
+    addMessage.value = ''
+})
+
+function displayMessages(){
+    let displayMessage = '';
+    if (todoList.length === 0){todo.innerHTML = ''}
+    todoList.forEach(function(item, i){
+        displayMessage += `
+        <li>
+            <input type="checkbox" id="item_${i}" ${item.checked ? 'checked' : ''}>
+            <label for="item_${i}" class="${item.important ? 'important' : ''}">${item.todo}</label>
+        </li>
+        `
+        todo.innerHTML = displayMessage;
+    })
+}
+
+todo.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id')
+    let forLabel = todo.querySelector('[for='+ idInput +']')
+    let valueLabel = forLabel.innerHTML
+
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked
+            localStorage.setItem('todo', JSON.stringify(todoList))
+        }
+    })
+})
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault()
+    todoList.forEach(function(item, i){
+        if (item.todo === event.target.innerHTML){
+            if (event.ctrlKey){
+                todoList.splice(i, 1)
+            } else {
+                item.important = !item.important
+            }
+            displayMessages()
+            localStorage.setItem('todo', JSON.stringify(todoList))
+        }
+    })
+})
